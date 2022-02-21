deb9c33f82cbc04a969d428d40c5aa903b737e58
uuko <omkkqq@gmail.com>
2022-02-21 21:56:20 +0800

    "room "

---
+++ app/src/main/java/com/example/modulepra/db/MainDBDataBase.java
@@ -0,0 +1,36 @@
+package com.example.modulepra.db;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {MainDBDataBase.class}, version = 1, exportSchema = false)
+public abstract class MainDBDataBase extends RoomDatabase {
+    private static final String DB_NAME = "alertDatabase.db";
+    private static volatile MainDBDataBase instance;
+
+    public static MainDBDataBase getInstance(Context context) {
+        if (instance == null) {
+            instance = create(context);
+        }
+        return instance;
+    }
+
+
+    private static MainDBDataBase create(final Context context) {
+        return Room.databaseBuilder(context, MainDBDataBase.class, DB_NAME)
+                .build();
+//          .addMigrations(MIGRATION_1_2)
+    }
+
+//    public static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+//        @Override
+//        public void migrate(@NonNull @NotNull SupportSQLiteDatabase database) {
+//            database.execSQL("ALTER TABLE EventDBTable " + "ADD COLUMN Note TEXT");
+//        }
+//    };
+
+    public abstract MainDbDao mainDbDao();
+}
---
+++ app/src/main/java/com/example/modulepra/db/MainDbDao.java
@@ -0,0 +1,37 @@
+package com.example.modulepra.db;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Update;
+
+import java.util.List;
+
+import io.reactivex.Completable;
+
+@Dao
+interface MainDbDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public Completable insertUsers(MainTable... users);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public Completable insertAllPrivateData(List<MainTable> friends);
+
+    @Update
+    public Completable updateUsers(MainTable... users);
+
+    @Delete
+    Completable deletePrivateDatas(List<MainTable> photoEntityList);
+
+    @Delete
+    public Completable deleteUsers(MainTable... users);
+
+
+//    @Query("SELECT * FROM PRIVATE_ENTITY WHERE qbee_id = :qbeeId AND owner = :owner" )
+//    Single<List<PhotoEntity>> loadAllPrivateDataByQbeeID(String qbeeId,String owner);
+//    @Query("SELECT * FROM PRIVATE_ENTITY")
+//    Single<List<PhotoEntity>> loadAllPrivateData();
+//    @Query("SELECT * FROM PRIVATE_ENTITY WHERE folder_content =(:folder_content) AND " + "item_id = (:item_id) LIMIT 1")
+//    Flowable<PhotoEntity> findOneUserNam(String folder_content,String item_id);
+}
